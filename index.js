e766de6fbc13b3af5c80c522a72baf1872f8a62d
Arra <145094087+minkiji@users.noreply.github.com>
2024-06-08 04:21:16 +0800

    v5

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// Fade in effects when scrolled
 const paragraphs = document.querySelectorAll(".we-offer, .service-offer, .hr-service, .sec-container, .featured-header, .featured-img, .faq-header, .animate-delay, .footer-outro, .footer-items-container, .faq");
 
 document.addEventListener("scroll", function () {
@@ -17,6 +19,9 @@ function isInView(element) {
   )
 }
 
+
+
+// Background color on navbar when scrolled
 document.addEventListener('scroll' , () => {
   const header = document.querySelector('header');
 
@@ -26,3 +31,53 @@ document.addEventListener('scroll' , () => {
     header.classList.remove('scrolled');
   }
 });
+
+
+
+// FAQs checkbox close when it opens another
+document.addEventListener('DOMContentLoaded', () => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+  checkboxes.forEach(checkbox => {
+      checkbox.addEventListener('change', () => {
+          if (checkbox.checked) {
+              checkboxes.forEach(otherCheckbox => {
+                  if (otherCheckbox !== checkbox) {
+                      otherCheckbox.checked = false;
+                  }
+              });
+          }
+      });
+  });
+});
+
+
+
+
+// Automatic slideshow
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+// Automatic slideshow
+setInterval(() => { showSlides(slideIndex += 1); }, 5000);
+
+
